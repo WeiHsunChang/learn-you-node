66d7308263336e0c98015b62036d0b42e23b4c86
WeiHsunChang <weihsun.code@gmail.com>
2020-02-03 17:28:08 +0800

    first commit

---
+++ asynIO.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+fs.readFile(process.argv[2],
+    function callback(err, data){
+        if(err == null){
+            const lines = data.toString().split('\n').length - 1
+            console.log(lines)          
+        }
+        else{
+            console.log(err)
+        }
+    })
